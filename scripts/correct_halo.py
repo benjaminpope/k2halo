99dedd8a1f926bdab4089a7e82722e58888d823a
Benjamin Pope <benjamin.pope@nyu.edu>
2019-03-06 10:02:51 -0800

    script

--- scripts/correct_halo.py
+++ scripts/correct_halo.py
@@ -163,7 +163,7 @@ if __name__ == '__main__':
     tab = fits.BinTableHDU.from_columns(cols)
 
     hdul = fits.HDUList([hdu, tab])
-    hdul.writeto('hlsp_halo_k2_llc_%s_-c%d.fits' % (epic,campaign),overwrite=True)
+    hdul.writeto('../release/hlsp_halo_k2_llc_%s_-c%d.fits' % (epic,campaign),overwrite=True)
 
     if args.do_plot:
-        plot_k2sc(lc,np.nanmean(tpf.flux,axis=0),f[0][:,:].T,formal_name=translate_greek(args.name).replace('_',' ')+' Detrended',save_file='%s_halo_k2sc.png' % args.name)
+        plot_k2sc(lc,np.nanmean(tpf.flux,axis=0),f[0][:,:].T,formal_name=translate_greek(args.name).replace('_',' ')+' Detrended',save_file='../release/%s_halo_k2sc.png' % args.name)
