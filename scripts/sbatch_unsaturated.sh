c20b03e8bc791f8ade04b5060fca42ac7dc4f612
Benjamin Pope <benjamin.pope@nyu.edu>
2019-05-04 19:03:00 -0400

    unsaturated

---
+++ scripts/sbatch_unsaturated.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=Array1d
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8GB
+#SBATCH --time=08:00:00
+#SBATCH --array=0-1754
+
+if [ "$SLURM_ARRAY_TASK_ID" == "" ]; then exit; fi
+
+i=0
+while read -r line; do
+    if [ $i -eq $SLURM_ARRAY_TASK_ID ]; then
+
+		module purge
+		module load anaconda3/5.3.1
+		source ~/pyenv/py3.7.0/bin/activate
+
+        eval $line 
+        exit
+    fi
+    i=$((i+1))
+done < sbatch_unsaturated.txt
+
+exit
