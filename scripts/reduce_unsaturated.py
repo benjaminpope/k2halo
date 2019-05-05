00fa5e9bfffee00b47bb0974f6c86b44181efd19
Benjamin Pope <benjamin.pope@nyu.edu>
2019-05-04 21:58:13 -0400

    commas

--- scripts/reduce_unsaturated.py
+++ scripts/reduce_unsaturated.py
@@ -9,7 +9,7 @@ with open('all_epics.txt','w') as f:
 	f.write('epic, cdpp_sap, cdpp_pdc, cdpp_k2sc_pdc, cdpp_halo, cdpp_k2sc_halo, enclosed_weight\n')
 
 	for fname in tqdm(files):
-		s = open(fname,'r').read()
+		s = open(fname,'r').read().replace(' ',', ')
 		f.write(s+'\n')
 
 print('Done!')
