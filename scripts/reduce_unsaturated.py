b2b904f8ed0521fff246dc56481a87e44b1b3f9b
Benjamin Pope <benjamin.pope@nyu.edu>
2019-05-04 21:55:06 -0400

    reduce unsaturated

---
+++ scripts/reduce_unsaturated.py
@@ -0,0 +1,15 @@
+import glob
+from astropy.table import Table 
+from tqdm import tqdm
+
+files = glob.glob('epic_*.txt')
+
+with open('all_epics.txt','w') as f:
+
+	f.write('epic, cdpp_sap, cdpp_pdc, cdpp_k2sc_pdc, cdpp_halo, cdpp_k2sc_halo, enclosed_weight\n')
+
+	for fname in tqdm(files):
+		s = open(fname,'r').read()
+		f.write(s+'\n')
+
+print('Done!')
